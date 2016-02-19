3f2df064fca56349c568a5a07eaa3359d2f103c8
Rahul Baphana <rahul.kbaphana@yahoo.com>
2016-02-19 12:48:06 +0530

    React router working! YAY

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -6,30 +6,15 @@ import Home from './components/homePage.jsx';
 import Authors from './components/authors/authorsPage.jsx';
 import About from './components/about/aboutPage.jsx';
 import Header from './components/common/header.jsx';
+import { Router, Route, hashHistory } from 'react-router'
 
-var App = React.createClass({
-	render: function() {
-		var Child;
-
-		switch(this.props.route){
-			case 'about': Child = About; break;
-			case 'authors': Child = Authors; break;
-			default : Child = Home;
-		}
-
-		return (
-			<div>
-				<Header/>
-				<Child/>
-			</div>
-		);
-	}
-});
-
-function render(){
-	var routeText = window.location.hash.substr(1);
-	ReactDOM.render(<App route={routeText}/>, document.getElementById('app'));
-}
-
-window.addEventListener('hashchange', render);
-render();
+ReactDOM.render((
+                  <div>
+                  <Header/>
+                   <Router history={hashHistory}>
+                      <Route path="/" component={Home}/>
+                      <Route path="/authors" component={Authors}/>
+                      <Route path="/about" component={About}/>
+                   </Router>
+                   </div>
+), document.getElementById('app'));
