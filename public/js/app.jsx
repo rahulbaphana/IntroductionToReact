3bb799e8de5109067f102b92aec4db69cd1e4197
Rahul Baphana <rahul.kbaphana@yahoo.com>
2016-02-19 13:13:36 +0530

    added routes file

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -2,19 +2,13 @@ import $ from 'jquery';
 import jQuery from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/homePage.jsx';
-import Authors from './components/authors/authorsPage.jsx';
-import About from './components/about/aboutPage.jsx';
 import Header from './components/common/header.jsx';
+import AllRoutes from './routes.js';
 import { Router, Route, hashHistory } from 'react-router'
 
 ReactDOM.render((
                   <div>
-                  <Header/>
-                   <Router history={hashHistory}>
-                      <Route path="/" component={Home}/>
-                      <Route path="/authors" component={Authors}/>
-                      <Route path="/about" component={About}/>
-                   </Router>
+                     <Header/>
+                     <AllRoutes/>
                    </div>
 ), document.getElementById('app'));
